85852751cbc92512b021fd7d47403af5255e1b60
Kyle Kingsbury <aphyr@aphyr.com>
2025-03-07 10:49:02 -0600

    Dependency bumps
    cheshire 5.13.0
    bifurcan-clj 0.1.3
    jepsen.history 0.1.5
    clojure 1.12.0
    data.fressian 1.1.10
    data.generators 1.1.0
    tools.cli 1.1.230

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/jepsen-io/history.sim"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[cheshire "5.12.0"]
-                 [com.aphyr/bifurcan-clj "0.1.1"]
-                 [io.jepsen/history "0.1.3"]
-                 [org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.fressian "1.0.0"]
-                 [org.clojure/data.generators "1.0.0"]
-                 [org.clojure/tools.cli "1.0.219"]]
+  :dependencies [[cheshire "5.13.0"]
+                 [com.aphyr/bifurcan-clj "0.1.3"]
+                 [io.jepsen/history "0.1.5"]
+                 [org.clojure/clojure "1.12.0"]
+                 [org.clojure/data.fressian "1.1.0"]
+                 [org.clojure/data.generators "1.1.0"]
+                 [org.clojure/tools.cli "1.1.230"]]
   :java-source-paths ["src/"]
   :main         jepsen.history.sim.cli
   :repl-options {:init-ns jepsen.history.sim}
